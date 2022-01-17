66c018867964fd470a8472cf419939bcd32dec17
Johannes Blühdorn <johannes.bluehdorn@scicomp.uni-kl.de>
2022-01-17 11:12:17 +0100

    License header update.

--- syntax/check.py
+++ syntax/check.py
@@ -1,6 +1,6 @@
 #  OpDiLib, an Open Multiprocessing Differentiation Library
 #
-#  Copyright (C) 2020-2021 Chair for Scientific Computing (SciComp), TU Kaiserslautern
+#  Copyright (C) 2020-2022 Chair for Scientific Computing (SciComp), TU Kaiserslautern
 #  Homepage: http://www.scicomp.uni-kl.de
 #  Contact:  Prof. Nicolas R. Gauger (opdi@scicomp.uni-kl.de)
 #
